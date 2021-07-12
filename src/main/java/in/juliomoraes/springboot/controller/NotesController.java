9acb7fe18ba74d383b21df3d2f4a0e74391c2bf3
julioribeiiro <jmoraes@daitan.com>
2021-07-12 02:00:33 -0300

    added the post request

--- src/main/java/in/juliomoraes/springboot/controller/NotesController.java
+++ src/main/java/in/juliomoraes/springboot/controller/NotesController.java
@@ -5,10 +5,7 @@ import in.juliomoraes.springboot.repository.NotesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,6 +19,12 @@ public class NotesController {
 
     @GetMapping("/notes")
     public ResponseEntity<List<Note>> readNotes (){
-        return new ResponseEntity<List<Note>>(notesRepository.findAll(), HttpStatus.OK);
+        return new ResponseEntity<>(notesRepository.findAll(), HttpStatus.OK);
+    }
+
+    @PostMapping("/notes")
+    public Note addNote(@RequestBody Note note) {
+        notesRepository.save(note);
+        return note;
     }
 }
